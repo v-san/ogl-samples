0123d1a4e300d4ba2e0a48cccdb635a74c1c3a36
FROL-HOME\FROL <vova@frolov.pp.ru>
2018-01-08 13:39:00 +0300

    add simplest triangle sample

---
+++ opengl_ex0_triangle/ShaderProgram.h
@@ -0,0 +1,45 @@
+#ifndef SHADERPROGRAM_H
+#define SHADERPROGRAM_H
+
+#include <unordered_map>
+#include "common.h"
+
+
+class ShaderProgram
+{
+public:
+
+  ShaderProgram() : shaderProgram(-1) {};
+
+  ShaderProgram(const std::unordered_map<GLenum, std::string> &inputShaders);
+
+  virtual ~ShaderProgram() {};
+
+  void Release(); //actual destructor
+
+  virtual void StartUseShader() const;
+
+  virtual void StopUseShader() const;
+
+  GLuint GetProgram() const { return shaderProgram; }
+
+
+  bool reLink();
+
+  void SetUniform(const std::string &location, float value) const;
+
+  void SetUniform(const std::string &location, double value) const;
+
+  void SetUniform(const std::string &location, int value) const;
+
+  void SetUniform(const std::string &location, unsigned int value) const;
+
+private:
+  static GLuint LoadShaderObject(GLenum type, const std::string &filename);
+
+  GLuint shaderProgram;
+  std::unordered_map<GLenum, GLuint> shaderObjects;
+};
+
+
+#endif
